6798cbfdf59086aa3e9c18efce7806009fc5121b
davidnso <davidnsoesie1@gmail.com>
2019-06-03 01:03:24 -0400

    add home page config

--- src/Home/home.js
+++ src/Home/home.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
-
+import './home.css'
+import SearchComponent from './components/SearchComponent'
+import TimeLineView from './components/TableView';
 export default class HomeComponent extends Component{
     render(){
         return(
-            <div>
-                <h1>HOME</h1>
-            </div>
+                <body className='homeWrapper'>
+                    <SearchComponent/>
+                    <TimeLineView/>
+                </body>
         )
     }
 }
